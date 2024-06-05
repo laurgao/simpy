68c1b56fc6357021d21cdc6df6f6ae19c21259da
Laura Gao <gaolauro@gmail.com>
2024-06-05 23:25:51 +0100

    fix bad pts tests

--- test_pts.py
+++ test_pts.py
@@ -5,9 +5,9 @@ from test_utils import *
 
 def test_sin_x_3():
     """Has been manually calculated <3"""
-    product_to_sum_unit(sin(x) ** 3) == sin(x) / 2 - sin(3 * x) / 4 + sin(x) / 4
+    assert product_to_sum_unit(sin(x) ** 3) == sin(x) / 2 - sin(3 * x) / 4 + sin(x) / 4
 
 
 def test_cos_x_5():
     """Has been manually calculated <3"""
-    product_to_sum_unit(cos(x) ** 5) == cos(5 * x) / 16 + cos(3 * x) / 16 + 3 * cos(x) / 8
+    assert product_to_sum_unit(cos(x) ** 5) == cos(5 * x) / 16 + 5 * cos(3 * x) / 16 + 5 * cos(x) / 8
